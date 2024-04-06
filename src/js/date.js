951595b8fb3906b836313d8d85526338b20040b4
Sergio <sergiopinpoio@gmail.com>
2024-04-05 20:19:02 -0500

    finished, bug fixed

--- src/js/date.js
+++ src/js/date.js
@@ -2,3 +2,8 @@ var date = new Date();
 	var current_date = date.getMonth()+1+"/"+ date.getDate()+"/"+date.getFullYear();
 	var date_time = current_date;	
 	document.getElementById("date").innerHTML = date_time;
+
+	var date = new Date();
+	var current_time = date.getHours()+":"+date.getMinutes();
+	var date_time = current_time;	
+	document.getElementById("hours").innerHTML = date_time;
--- src/js/hour.js
+++
@@ -1,4 +0,0 @@
-var date = new Date();
-	var current_time = date.getHours()+":"+date.getMinutes();
-	var date_time = current_time;	
-	document.getElementById("hours").innerHTML = date_time;
